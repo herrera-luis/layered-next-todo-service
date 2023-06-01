b25f0588331848e58965acc5d22456821c4cadbc
herrera-luis <herrera_luis10@hotmail.com>
2023-05-31 21:47:04 -0500

    feat: using latest todo endpoints

--- app/services/TodoService.ts
+++ app/services/TodoService.ts
@@ -4,19 +4,19 @@ import { Todo } from "../models/Todo";
 const API_BASE_URL = "http://localhost:8000";
 
 export const fetchTodos = async (): Promise<Todo[]> => {
-    const { data } = await axios.get(`${API_BASE_URL}/api/v1/todos/`);
+    const { data } = await axios.get(`${API_BASE_URL}/api/v1/todo`);
     return data;
 };
 
 export const addTodo = async (todo: Todo): Promise<Todo> => {
-    const { data } = await axios.post(`${API_BASE_URL}/api/v1/todos/`, todo);
+    const { data } = await axios.post(`${API_BASE_URL}/api/v1/todo`, todo);
     return data;
 };
 
 export const updateTodo = async (todo: Todo): Promise<void> => {
-    await axios.put(`${API_BASE_URL}/api/v1/todos/${todo.id}`, todo);
+    await axios.put(`${API_BASE_URL}/api/v1/todo/${todo.id}`, todo);
 };
 
 export const deleteTodo = async (id: number): Promise<void> => {
-    await axios.delete(`${API_BASE_URL}/api/v1/todos/${id}`);
+    await axios.delete(`${API_BASE_URL}/api/v1/todo/${id}`);
 }
