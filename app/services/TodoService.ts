e44d30cb5cf76b585c8c7f2d4608fcf99a454fe9
herrera-luis <herrera_luis10@hotmail.com>
2023-08-04 22:24:37 -0500

    feat: updating service endpoints

--- app/services/TodoService.ts
+++ app/services/TodoService.ts
@@ -5,19 +5,19 @@ import { env } from '../lib/utils';
 const API_BASE_URL = env('API_BASE_URL') || process.env.API_BASE_URL;
 
 export const fetchTodos = async (): Promise<Todo[]> => {
-    const { data } = await axios.get(`${API_BASE_URL}/api/v1/todo`);
+    const { data } = await axios.get(`${API_BASE_URL}/api/v1/todos`);
     return data;
 };
 
 export const addTodo = async (todo: Todo): Promise<Todo> => {
-    const { data } = await axios.post(`${API_BASE_URL}/api/v1/todo`, todo);
+    const { data } = await axios.post(`${API_BASE_URL}/api/v1/todos`, todo);
     return data;
 };
 
 export const updateTodo = async (todo: Todo): Promise<void> => {
-    await axios.put(`${API_BASE_URL}/api/v1/todo/${todo.id}`, todo);
+    await axios.put(`${API_BASE_URL}/api/v1/todos/${todo.id}`, todo);
 };
 
 export const deleteTodo = async (id: number): Promise<void> => {
-    await axios.delete(`${API_BASE_URL}/api/v1/todo/${id}`);
+    await axios.delete(`${API_BASE_URL}/api/v1/todos/${id}`);
 }
